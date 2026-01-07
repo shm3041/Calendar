65416fd7f776a4644766214e9dbe78880da00c9f
shm3041 <shm3041@naver.com>
2026-01-08 01:21:50 +0900

    Update

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/calendar")
+    public String calendar() {
+        return "calendar";
+    }
+}
